af043498c5da0097ba371265edc7f1b0eb0453ac
sara khalil <sarakhalil3377@gmail.com>
2022-10-15 16:21:02 +0300

    DONE

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,23 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Directors Page</h1>
+    {directors.map((director, i) => {
+      return (
+        <div key={i}>
+          <h3>Name:{director.name}</h3>
+          <p>Movies:</p>
+          <ul>
+            {director.movies.map((movie,i) => {
+              return(
+                <li key={i}>{movie}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })}</div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return <div>
+    <h1>Movies Page</h1>
+    { movies.map((movie,i) => {
+      return (
+        <div key={i}>
+          <h3>Name: {movie.title}</h3>
+          <p>Time:{movie.time}</p>
+          <p>Genres:</p>
+          <ul>
+            {movie.genres.map((genre,i) => {
+              return(
+                <li key={i}>{genre}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })}
+    </div>;
 }
 
 export default Movies;
